906001aa890792cca9004e389443d5ece4caa16c
Alfarizkyyy <alfarizkyc091@gmail.com>
2025-07-11 19:56:32 +0700

    kedua

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -19,7 +19,7 @@ Kirim Email
 300">LinkedIn</a>
 
 </div>
-<p class="text-gray-500">&copy; {{ new Date().getFullYear() }} Nama Mahasiswa. Dibuat dengan
+<p class="text-gray-500">&copy; {{ new Date().getFullYear() }} RM Alfarizky CK. Dibuat dengan Penuh Cinta
 </p>
 </div>
 </footer>
